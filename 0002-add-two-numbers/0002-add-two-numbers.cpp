cb60f9cbd215ac537fac81009efac3ab5aedc284
eungyung <53503771+keg9335@users.noreply.github.com>
2023-07-23 10:21:37 +0900

    Time: 45 ms (33.99%), Space: 71.3 MB (68.00%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,14 +14,12 @@ public:
         int val = 0, carry = 0;
         ListNode* result = new ListNode();
         ListNode* current = result;
-        ListNode* nullNode = new ListNode();
         
         while (l1 || l2 || carry) {
             current->next = new ListNode();
             current = current->next;
             int val1 = l1?l1->val:0, val2 = l2?l2->val:0;
-            val = (val1 + val2 + carry) % 10;
-            current->val = val;
+            current->val = (val1 + val2 + carry) % 10;
             carry = (val1 + val2 + carry) / 10;
             l1 = l1? l1->next : nullptr;
             l2 = l2? l2->next : nullptr;
