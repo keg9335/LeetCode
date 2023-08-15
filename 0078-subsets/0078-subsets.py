d929b93056c1fe319a44d12d0bd9260e920baa03
eungyung <53503771+keg9335@users.noreply.github.com>
2023-08-15 18:02:15 +0900

    Time: 17 ms (67.05%), Space: 13.5 MB (64.72%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,8 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        result = []
-        for i in range(len(nums) + 1) :
-            result.extend(list(itertools.combinations(nums, i)))
-            
+        # result = []
+        # for i in range(len(nums) + 1) :
+        #     result.extend(list(itertools.combinations(nums, i)))
+        
+        result = list(j for i in range(len(nums) + 1) for j in itertools.combinations(nums, i) )
+        
         return result
