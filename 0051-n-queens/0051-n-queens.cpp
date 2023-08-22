8daa37052abdd00fe566cd2824cd69cec94e32a0
eungyung <53503771+keg9335@users.noreply.github.com>
2023-08-22 20:53:00 +0900

    Time: 11 ms (37.23%), Space: 16.6 MB (18.42%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> board;
+        string s = "";
+        for (int i = 0; i < n; i++) {
+            s.push_back(' ');
+        }
+        vector<string> ex(n,s);
+        
+        solveQueens(n, 0, board, ex);
+
+        return board;
+    }
+    
+    void solveQueens(int n, int curr, vector<vector<string>>& board, vector<string> ex) {
+        if (curr == n) { 
+            board.push_back(ex);
+            return;
+        }
+           
+        auto idx = ex[curr].find(' ', 0);
+
+        while (idx != string::npos) {
+            vector<string> exSub(ex);
+            exSub[curr][idx] = 'Q';
+            operateQueen(n, curr, idx, exSub);
+            solveQueens(n, curr+1, board, exSub);
+            idx = ex[curr].find(' ', idx+1);
+        }      
+    }
+    
+    void operateQueen(int n, int curr, int idx, vector<string>& ex) {
+        for (int i = 0; i < n; i++) {
+            if (i != idx)
+                ex[curr][i] = '.';
+            if (ex[i][idx] == ' ')
+                ex[i][idx] = '.';
+        
+            if (i != curr) {
+                int res = curr > i? curr - i : i- curr;
+                if (idx - res >= 0 && ex[i][idx-res] == ' ') 
+                    ex[i][idx-res] = '.';
+                if (idx + res < n && ex[i][idx+res] == ' ')
+                    ex[i][idx+res] = '.';
+            }
+        }
+    }
+};
