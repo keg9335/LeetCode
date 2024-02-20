7f1519bffee258995ae29bb86482f4b772d66a10
eungyung <53503771+keg9335@users.noreply.github.com>
2024-02-20 13:32:18 +0900

    Time: 52 ms (85.71%), Space: 6.9 MB (100.00%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.c
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.c
@@ -10,12 +10,13 @@ int countPalindromicSubsequence(char* s) {
     int cnt = 0, i = 0, j = 0, letters = 0;
     int len = strlen(s);
     char visited[26] = {0,};
+    int endidx = 0;
 
     for (i = 0; i < len - 1; i++) {
         if (visited[s[i]-'a']) continue;
         int frequency[26] = {0,};
-        int endidx = 0;
         visited[s[i]-'a'] = 1;
+        endidx = 0;
         for (j = len-1; j > i; j--) {
             if (endidx)
                 frequency[s[j] - 'a']++;
