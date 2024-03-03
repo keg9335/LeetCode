803507e28327ff3dcfde21e8fd59519b66cc3845
eungyung <53503771+keg9335@users.noreply.github.com>
2024-03-03 15:51:06 +0900

    Time: 468 ms (57.49%), Space: 31.3 MB (91.87%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -11,9 +11,7 @@ class Solution:
             
         for i in range(len(s)):
             if self.palindrome(s[:i+1]):
-                element.append(s[:i+1])
-                self.backtrack(s[i+1:], element[:], result)
-                element.pop()
+                self.backtrack(s[i+1:], element + [s[:i+1]], result)
     
     def palindrome(self, s):
         return s == s[::-1]
