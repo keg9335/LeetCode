b678e6b2eb454bf961b3ea068fd61cb877f445ac
eungyung <53503771+keg9335@users.noreply.github.com>
2024-03-04 15:50:26 +0900

    Time: 52 ms (52.94%), Space: 7.4 MB (29.41%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,25 @@
+int countPalindromicSubsequence(char* s) {
+    int cnt = 0, i = 0, j = 0;
+    int len = strlen(s);
+    char visited[26] = {0,};
+    int endidx = 0;
+
+    for (i = 0; i < len - 1; i++) {
+        if (visited[s[i]-'a']) continue;
+        int frequency[26] = {0,};
+        visited[s[i]-'a'] = 1;
+        endidx = 0;
+        for (j = len-1; j > i; j--) {
+            if (endidx)
+                frequency[s[j] - 'a']++;
+            else if (s[j] == s[i]) endidx = j;
+        }
+        if (endidx > i+1) {
+            for (j = 0 ; j < 26; j++) {
+                if (frequency[j]) cnt++;
+            }
+        }
+    }
+  
+    return cnt;
+}
