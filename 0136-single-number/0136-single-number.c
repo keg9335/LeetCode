e455a9a128da4dfc0c6265d741b59f79e32a505f
eungyung <53503771+keg9335@users.noreply.github.com>
2024-03-05 16:09:24 +0900

    Time: 845 ms (16.22%), Space: 6.8 MB (99.08%) - LeetHub

---
+++ 0136-single-number/0136-single-number.c
@@ -0,0 +1,14 @@
+int singleNumber(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        int cnt = 0;
+        int flag = 0;
+        for (int j = 0; j < numsSize; j++) {
+            if (i != j && nums[i] == nums[j]){
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) return nums[i];
+    }
+    return NULL;
+}
